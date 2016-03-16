3b654bf70e64a1aea44fd019ec1d402570c34430
danozgriff <dane.griff@gmail.com>
2016-03-16 22:02:32 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -86,7 +86,7 @@ def ScrapeBritishMain():
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
     #response = br.open(url + tidm)
-    response = br.open(url + tidm)
+    response = br.open(url)
 
     for pagenum in range(1):
         html = response.read()
