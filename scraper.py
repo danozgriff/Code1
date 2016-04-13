c687cbba5311ec7b370267344b7169e7dd6b2c45
danozgriff <dane.griff@gmail.com>
2016-04-13 21:11:02 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,63 +10,6 @@ import datetime
 ##################################################      
 #Load Prices from shareprices.com
 ##################################################
-
-def NewLivePrices():
-
-    print "1"
-    url = 'http://shares.telegraph.co.uk/indices/?index=SMX'
-    
-    br = mechanize.Browser()
-    
-        # sometimes the server is sensitive to this information
-    #br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-    
-    scraperwiki.sqlite.execute("drop table if exists company")  
-    scraperwiki.sqlite.execute("create table company (`TIDM` string, `Company` string, `Price` real, `Volume` real, `Date` date NOT NULL)")
-
-    #scraperwiki.sqlite.execute("delete from company")
-    #scraperwiki.sqlite.commit()
-    
-    print "2"
-    response = br.open(url)
-    print "3"
-    
-    for pagenum in range(1):
-        print "4"
-        html = response.read()
-        print "5"
-        #print html
-        test1 = re.search(r'tablesorter full(.*?)</tbody>', html).group(0)
-        #print test1
-        tuples = re.findall(r'(">)(.*?)</td>', str(test1)) #.replace(" ", "")).replace("><", "")
-        count = 0
-        tidm = ""
-        company = ""
-        price = 0
-        poscnt = 0
-        for tuple in tuples:
-            print tuple[1]
-            #if poscnt == 1:
-            #    company = tuple[1].replace("amp;", "")
-            #if poscnt == 2:
-            #    price = float(tuple[1].replace(",", "").replace("p", ""))
-            #if poscnt == 4:
-            #    scraperwiki.sqlite.save(["TIDM"], data={"TIDM":tidm+'.L', "Company":company, "Price":price, "Volume":tuple[1].replace(",", ""), "Date":datetime.date.today()}, table_name='company')
-            #    scraperwiki.sqlite.commit()
-            #if len(tuple[1]) <= 4 and tuple[1][-1:].isalpha() and tuple[1][-1:].isupper() and tuple[1]!=tidm and poscnt!=1:
-            #    count = count+1
-            #    tidm = tuple[1]
-            #    poscnt = 1
-            #else:
-            #    poscnt = poscnt + 1    
-            
-        #print "%s ftseallshare records were loaded" % (count)
-
-        return;
-
-##################################################      
-#Load Prices from shareprices.com
-##################################################
 #
 
 def ScrapeLivePrices():
@@ -411,6 +354,8 @@ def SignalPerformance():
 if __name__ == '__main__':
     
     #ScrapeBritishMain()
-    NewLivePrices()
+    #NewLivePrices()
     #ScrapeLivePrices()
     #SignalPerformance()
+    
+    print datetime.datetime.time(datetime.datetime.now())
