55a922ea8c70c10330160d8a3453205849d9500a
danozgriff <dane.griff@gmail.com>
2016-04-13 21:32:42 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,11 +19,11 @@ def ScrapeLivePrices():
     for ftse in ftses:        # Second Example
 
         if ftse == 'FTSE 100':
-        url = 'http://shareprices.com/ftse100'
+            url = 'http://shareprices.com/ftse100'
         elsif ftse == 'FTSE 250':
-        url = 'http://shareprices.com/ftse250'
+            url = 'http://shareprices.com/ftse250'
         elsif ftse == 'FTSE Small Cap':
-        url = 'http://shareprices.com/ftsesmallcap'
+            url = 'http://shareprices.com/ftsesmallcap'
         
         br = mechanize.Browser()
         
