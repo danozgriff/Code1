5e7226927b866c1352d2a76acb51eb437683a72a
danozgriff <dane.griff@gmail.com>
2017-02-23 12:51:00 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -388,7 +388,7 @@ def ScrapeSignalHistory(runno):
         lselist = scraperwiki.sqlite.execute("select distinct `tidm` from company where substr(tidm,1,1) in ('C', 'J', 'L', 'Q', 'X') and tidm not in ('%s')" % (CoreSQL))  
       elif weekday == 3:
         #lselist = scraperwiki.sqlite.execute("select distinct `tidm` from company where substr(tidm,1,1) in ('D', 'K', 'R', 'Y') and tidm not in ('%s')" % (CoreSQL))  
-        lselist = scraperwiki.sqlite.execute("select distinct `tidm` from company where substr(tidm,1,1) in ('Z')")
+        lselist = scraperwiki.sqlite.execute("select distinct `tidm` from company where substr(tidm,1,1) in ('2')")
       elif weekday == 4:
         lselist = scraperwiki.sqlite.execute("select distinct `tidm` from company where substr(tidm,1,1) in ('E', 'S', 'Z') and tidm not in ('%s')" % (CoreSQL))  
       elif weekday == 5:
