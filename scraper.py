e0c00691ff67f5013bfb27c56e841520568f0dc0
danozgriff <dane.griff@gmail.com>
2017-04-04 21:21:52 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -597,7 +597,7 @@ def ScrapeUserInput():
     #print html
     #test1 = re.search(r'content=\"Sheet1(.*?)\"><meta name=\"google\" content=\"notranslate\">', html).group()
     #test1 = re.search(r'Earnings((.|\n)+)\"><meta name=\"google\"', html).group()
-    test1 = re.search(r'ltr\">((.|\n)+)<\\td>', html).group()
+    test1 = re.search(r'"ltr">((.|\n)+)</td>', html).group()
     test2 = test1.replace(". ", ".").replace("/ ", "/")
     print test2
     test3 = re.findall(r'(.*?)\,', test2)
