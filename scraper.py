2e4e56c3b089e97b616134e7069dfe62d2067506
danozgriff <dane.griff@gmail.com>
2017-05-04 15:20:33 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -439,51 +439,8 @@ def ScrapeSignalHistory(runno):
         #pause before calling the URL
         if runno <= 1:
           time.sleep(random.uniform(5, 10))
-        elif runno == 2:
-          time.sleep(random.uniform(10, 45))
-          ### CALL PRICE HISTORY FUNCTION ####
           ScrapePriceHistory(tidm)
 
-        sighistidm = 'z'
-        sighistcnt = 0
-
-        response = br.open(url + tidm)
-        if sighistidm == tidm:
-          sighistcnt = sighiscnt + 1
-          #print "Price History TIDM: %s Count: %d" % (sighistidm, sighiscnt)
-        elif sighistidm != tidm:
-          sighistidm = tidm
-          sighiscnt = 0
-        #debugcnt = debugcnt + 1
-    
-    #for pagenum in range(1):
-        html = response.read()
-
-        test1 = re.search(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)MainContent_signalpagehistory_PatternHistory24_IADD', html)
-
-        if test1:
-            test1 = test1.group(0)
-
-            test3 = re.findall('(\">|img\/)(.*?)(<\/|\.gif)', test1.replace("\B", ""))
-
-            while len(test3) >= 5:
-
-                sh_Date = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
-                sh_Date = date(int(sh_Date[6:10]),int(sh_Date[3:5]),int(sh_Date[:2]))
-                #print "Date: %s" % (sh_Date)
-                sh_Price = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "").replace(",", "")).group(0)
-                #print "Price: %s" % (sh_Price)
-                sh_Signal = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
-                #print "Signal: %s" % (sh_Signal)
-                sh_Confirmation = ((re.search("[Uncheck|Check]", str(test3.pop(0)).replace(" ", "")).group(0).lower()).replace("u","N")).replace("c", "Y")
-                #print "Confirmation: %s" % (sh_Confirmation)
-                sh_GBP100 = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "").replace(",", "")).group(0)
-                #print "GDP100: %s" % (sh_GBP100)
-                #print "Rundate: %s" % rundate
-
-                scraperwiki.sqlite.execute("insert or ignore into Signal_History values (?, ?, ?, ?, ?, ?, ?)",  [tidm, sh_Date, sh_Price, sh_Signal, sh_Confirmation, sh_GBP100, rundate]) 
-
-                scraperwiki.sqlite.commit()
                     
     #print "debugcnt: %d" % (debugcnt)
     return;
@@ -1092,9 +1049,9 @@ if __name__ == '__main__':
     #print "%s Scraping Live Prices.." % (datetime.datetime.utcnow() + timedelta(hours=8))
     #ScrapeLivePrices()
 
-    #Logger(rundt, 'ScrapeSignalHistory_Core', None)
-    #print "%s Scraping Signal History (Core).." % (datetime.datetime.utcnow() + timedelta(hours=8))
-    #ScrapeSignalHistory(0)
+    Logger(rundt, 'ScrapeSignalHistory_Core', None)
+    print "%s Scraping Signal History (Core).." % (datetime.datetime.utcnow() + timedelta(hours=8))
+    ScrapeSignalHistory(0)
 
     #Logger(rundt, 'UpdateOpenTrades', None)
     #print "%s Updating Open Trades.." % (datetime.datetime.utcnow() + timedelta(hours=8))
